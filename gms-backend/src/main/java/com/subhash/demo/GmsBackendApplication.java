45a5c60bdebe9f1788acccb1fb1d6f390fff3611
SubhashRavindraSRIVATSA <srs.programming@gmail.com>
2023-10-27 21:28:05 +0200

    initial commit

---
+++ gms-backend/src/main/java/com/subhash/demo/GmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.subhash.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmsBackendApplication.class, args);
+	}
+
+}
